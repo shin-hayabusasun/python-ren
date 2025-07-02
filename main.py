8230c2634ee915974c59d349584fe9930facf9e6
shin-hayabusasun <156934097+shin-hayabusasun@users.noreply.github.com>
2025-07-02 22:47:09 +0900

    a

---
+++ main.py
@@ -0,0 +1,38 @@
+import csv
+import numpy as np
+import datetime
+from commit_func import make_time_git_commit
+
+
+def get_commit_pattern(file_name):
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        commit_pattern = list(reader)
+    commit_pattern = np.array(commit_pattern)
+    return commit_pattern.T
+
+
+def change_file(file_name):
+    with open(file_name, "a") as f:
+        f.write("a")
+
+
+def main():
+    pattern_file = "pattern.csv"
+    commit_pattern = get_commit_pattern(pattern_file)
+
+    # 描画するパターンの中で最も日にちが早い箇所の基準点を設定
+    START_YEAR  = 2025
+    START_MONTH = 6
+    START_DAY   = 29
+
+    d = datetime.date(START_YEAR, START_MONTH, START_DAY)
+    for b in commit_pattern.reshape(-1):
+        if b == "1":
+            change_file("commit_file")
+            make_time_git_commit(d)
+        d += datetime.timedelta(days=1)
+
+
+if __name__ == "__main__":
+    main()
